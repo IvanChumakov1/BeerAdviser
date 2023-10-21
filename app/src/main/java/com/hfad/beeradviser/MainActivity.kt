0eeb9e5cb57a6adf185e7b34b36ab2900857c5f8
IvanChumakov1 <ivanchumakov228@gmail.com>
2023-10-21 07:00:55 +0700

    Recommendation Beer

--- app/src/main/java/com/hfad/beeradviser/MainActivity.kt
+++ app/src/main/java/com/hfad/beeradviser/MainActivity.kt
@@ -15,7 +15,15 @@ class MainActivity : AppCompatActivity() {
             val beerColor = findViewById<Spinner>(R.id.color)
             val color = beerColor.selectedItem
             val brands = findViewById<TextView>(R.id.brands)
-            brands.text = "Beer color is $color"
+            brands.text = "Beer color is $color" + "\n" + GetBeers(color.toString())
+        }
+    }
+    fun GetBeers(color: String): List<String>{
+        return when (color){
+            "Light" -> listOf("Jail Pale Ale", "Lager Lite")
+            "Amber" -> listOf("Jack Amber", "Red Moose")
+            "Brown" -> listOf("Brown Ber Beer", "Bock Brownie")
+            else -> listOf("Gout Stout", "Dark Daniel")
         }
     }
 }
